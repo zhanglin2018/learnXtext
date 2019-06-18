546fc399b3bfb4e09ba135cdb210342dfbdf9f48
Laughing Zhang <lin.zhang@advantest.com>
2019-06-18 13:48:08 +0800

    add some items.

---
+++ org.example.entities.parent/EWC_runtime_entities.examples/src-gen/entities/HaiSheng.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class HaiSheng {
+	private String name;
+	private String sex;
+	
+	public String getName(){
+		return name;
+	}
+		
+	public void setName(String _arg){
+		this.name = _arg;
+	}
+	
+	public String getSex(){
+		return sex;
+	}
+		
+	public void setSex(String _arg){
+		this.sex = _arg;
+	}
+	
+}
